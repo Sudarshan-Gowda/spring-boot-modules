d0496a17be2ca313208867affeeafd2858c0b8a6
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-12-12 19:16:37 +0530

    File changes

--- rest-api-mysql-hibernate-mapping/src/main/java/com/star/sud/mapping/repository/UserRespository.java
+++ rest-api-mysql-hibernate-mapping/src/main/java/com/star/sud/mapping/repository/UserRespository.java
@@ -1,5 +1,7 @@
 package com.star.sud.mapping.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,5 +12,7 @@ public interface UserRespository extends JpaRepository<User, String> {
 
 	@Query("SELECT count(*) FROM User where userName = :userName or email = :email")
 	public int validateUserContrainsts(@Param("userName") String userName, @Param("email") String email);
+
+	public List<User> findByUserNameOrEmail(String userName, String email);
 
 }
